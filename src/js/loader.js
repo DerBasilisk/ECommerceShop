ad76c2bed542273083ef034c778d4fab53db0a71
Ambiente 106 <ambiente106@iMac-de-CADPH.local>
2025-11-18 17:30:49 -0500

    coito

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,14 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-    function loadComponent(selector, file) {
-      fetch(file)
-        .then(response => response.text())
-        .then(data => {
-          document.querySelector(selector).innerHTML = data;
-        })
-        .catch(error => console.error("Error cargando:", file, error));
-    }
-  
-    loadComponent("header", "../pages/partials/header.html");
-    loadComponent("footer", "../pages/partials/footer.html");
-  });
-  
+  function loadComponent(selector, file) {
+    fetch(file)
+      .then(response => response.text())
+      .then(data => {
+        document.querySelector(selector).insertAdjacentHTML("beforeend", data);
+      })
+      .catch(error => console.error("Error cargando:", file, error));
+  }
+
+  loadComponent("header", "../pages/partials/header.html");
+  loadComponent("footer", "../pages/partials/footer.html");
+});
