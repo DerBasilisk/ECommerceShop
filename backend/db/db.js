8275f2a63f6ac4d6d4d86684e0242c8f31ab143c
Ambiente 106 <ambiente106@iMac-de-CADPH.local>
2025-12-02 17:03:55 -0500

    auto-update

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,5 +1,9 @@
 import mongoose, { Mongoose } from "mongoose"
-const uri ="mongodb+srv://adsotarde:adso2025@ecommerce.vawlahy.mongodb.net/TIENDA?retryWrites=true&w=majority";
+import dotenv from "dotenv"
+
+dotenv.config();
+
+const uri =process.env.MONGODB_URI;
 mongoose.connect(uri)
 .then(()=>console.log("✅┬─┬ノ(ಠ_ಠノ) Conectado a la base de datos"))
 .catch(err=>console.log("❌(╯°□°)╯︵ ┻━┻ Error al conectar a la base de datos",err));
